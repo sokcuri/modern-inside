cf9cb894fbb89ac44e0844fd26349c76f4554437
sokcuri <sokcuri@gmail.com>
2021-05-03 06:00:36 +0900

    feat: implement prepare for XHR when clicking on a tab - add ListKindTab enum - add makeListUrl function

--- src/components/pc/page_head/ListTopArrayTab.svelte
+++ src/components/pc/page_head/ListTopArrayTab.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
 import { getStore } from '@/store';
 
-const listTypeFilter = getStore<number>('count', 0);
+enum ListKindTab {
+    All = 'all',
+    Recommend = 'recommend',
+    Notice = 'notice'
+}
+
+const listTypeFilter = getStore<string>('listType', ListKindTab.All);
+
+function makeListUrl(sType: string, bType: string) {
+    var arrExceptionType = ["all","recommend","notice","movie","gallhit"];
+	if (arrExceptionType.indexOf(sType) === -1) {
+		return false;
+	}
+
+    const list_url = document.querySelector<HTMLInputElement>('#list_url')?.value;
+
+	var params = ((bType != "list")?"&board_type="+bType:"");
+		params += ((sType != "all")?"&exception_mode="+sType:"");
+
+	var url = list_url + params;
+	return url;
+}
+
+function onClickListKindTab(listKindTab: ListKindTab) {
+    const url = makeListUrl(listKindTab, 'list');
+
+    listTypeFilter.update(_ => listKindTab);
+    console.log('target url', url);
+}
 </script>
 
-<button type="button" class={$listTypeFilter === 0 ? 'on' : '' } on:click={_ => listTypeFilter.update(_ => 0)}>전체글</button>
-<button type="button" class={$listTypeFilter === 1 ? 'on' : '' } on:click={_ => listTypeFilter.update(_ => 1)}>개념글</button>
-<button type="button" class={$listTypeFilter === 2 ? 'on' : '' } on:click={_ => listTypeFilter.update(_ => 2)}>공지</button>
+<button type="button" class={$listTypeFilter === ListKindTab.All ? 'on' : '' } on:click={_ => onClickListKindTab(ListKindTab.All)}>전체글</button>
+<button type="button" class={$listTypeFilter === ListKindTab.Recommend ? 'on' : '' } on:click={_ => onClickListKindTab(ListKindTab.Recommend)}>개념글</button>
+<button type="button" class={$listTypeFilter === ListKindTab.Notice ? 'on' : '' } on:click={_ => onClickListKindTab(ListKindTab.Notice)}>공지</button>
 <!--<button type="button" class="" onclick="listKindTab('movie','list');return false;">동영상</button>-->
